c3aecd601b97d3bc4167e7621414211071bd45df
Sebastian Andres Trigos <112731508+20201025113sebas@users.noreply.github.com>
2022-11-02 17:38:12 -0500

    Add files via upload

---
+++ Clases.py
@@ -0,0 +1,38 @@
+class Persona:
+    def __init__(self):
+        self.bigote
+        self.nombre
+        self.edad
+        self.genero
+        self.volumen
+
+    def gritar ():
+        pass
+    def bailar ():
+        pass        
+    def saltar ():
+        pass
+
+sujeto_a =Persona()
+sujeto_a.nombre = "Camilo"
+sujeto_a.bigote = "False"
+sujeto_a.edad = 20
+sujeto_a.genero = "pansexual"
+sujeto_a.volumen = "alto"
+
+sujeto_b =Persona()
+sujeto_b.nombre = "Cristian"
+sujeto_b.bigote = "False"
+sujeto_b.edad = 56
+sujeto_b.genero = "masculino"
+sujeto_b.volumen = "medio"
+
+sujeto_c =Persona()
+sujeto_c.nombre = "Lina"
+sujeto_c.bigote = "False"
+sujeto_c.edad = 19
+sujeto_c.genero = "femenino"
+sujeto_c.volumen = "bajo"
+
+
+
